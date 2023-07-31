5b4e2242602da9760fb30a3a07e3c4db07551fc8
stevanakva <98061128+stevanakva@users.noreply.github.com>
2023-07-31 12:10:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ app.post('/api/notes', (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env || 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
