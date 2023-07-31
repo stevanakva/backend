c377c4f0303d4c0c0cceb1d43f6cc7dbcd948616
stevanakva <stevanakvadrat@gmail.com>
2023-07-31 14:17:25 +0200

    New index.js

--- index.js
+++ index.js
@@ -1,110 +1,102 @@
-
-const express = require('express');
-const cors = require('cors');
-const app = express();
-
+const express = require('express')
+const app = express()
+const cors = require('cors')
 
 const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method);
-    console.log('Path:', request.path);
-    console.log('Body:', request.body);
-    console.log('----');
-    next();
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
 }
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({error:'unknown endpoint'})
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
-app.use(cors());
-app.use(express.json());
-app.use(express.static('build'));
-app.use(requestLogger);
+app.use(cors())
+app.use(express.json())
+app.use(requestLogger)
+app.use(express.static('build'))
 
-let notes =[
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of the HTTP protocol",
-        important: true
-    }
- ]
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello world!<h1>');
-});
 
-app.get('/api/notes', (request, response) => {
-    response.json(notes);
-});
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);    
-    const note = notes.find(note => note.id === id);
-    if(note){
-        response.json(note);
-    } else {
-        response.status(404).end();
-    }   
-    
-});
-
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    notes  = notes.filter(note => note.id !== id);
-    console.log(notes);
-    response.status(204).end();
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
 })
 
 const generateId = () => {
-    const maxId = notes.length > 0
+  const maxId = notes.length > 0
     ? Math.max(...notes.map(n => n.id))
-    : 0;
-    return maxId + 1;
+    : 0
+  return maxId + 1
 }
 
 app.post('/api/notes', (request, response) => {
-    const body = request.body;
+  const body = request.body
 
-    if(!body.content){
-        return response.status(400).json({
-            error: 'content missing'
-        });
-    }
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
 
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        id: generateId(),
-    }
-    
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
 
-    notes = notes.concat(note);
+  notes = notes.concat(note)
 
-    response.json(note);
+  response.json(note)
 })
 
-app.use(unknownEndpoint);
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
 
-<<<<<<< HEAD
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 
+  response.json(note)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-=======
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
->>>>>>> 5b4e2242602da9760fb30a3a07e3c4db07551fc8
