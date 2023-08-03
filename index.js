8055b8793efd5653b79fe99eb9680893ea44d2c8
stevanakva <stevanakvadrat@gmail.com>
2023-08-03 16:17:20 +0200

    Izmene

--- index.js
+++ index.js
@@ -62,35 +62,29 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({ 
       error: 'content missing' 
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
+        
+  });
 
-  notes = notes.concat(note)
- 
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+   
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
-
-  response.json(note)
+  Note.findById(request.params.id).then(note =>{
+    response.json(note);
+  });
+  
 })
 
 app.delete('/api/notes/:id', (request, response) => {
