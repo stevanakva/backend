4d1b696538dcc4a38b33595a0eefbf4e4a151a68
stevanakva <stevanakvadrat@gmail.com>
2023-08-05 09:57:59 +0200

    izmene

--- index.js
+++ index.js
@@ -107,7 +107,10 @@ app.put('/api/notes/:id', (request, response, next) => {
 
  
 
- Note.findByIdAndUpdate(request.params.id, {content, important}, {new: true, runValidators: true, context: 'query'})
+ Note.findByIdAndUpdate(
+  request.params.id, 
+  {content, important},
+   {new: true, runValidators: true, context: 'query'})
   .then(updatedNote => {
     response.json(updatedNote)
   })
